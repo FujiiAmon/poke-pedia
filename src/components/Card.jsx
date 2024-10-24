b5d23e5df1a1544935acf87d73e43f92f7a0cabb
FujiiAmon <amondealwis@gmail.com>
2024-10-24 17:57:25 +0900

    これがasync内での解決

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export const Card = ({ pokemon }) => {
+    return (
+        <div>
+            <h1>Card</h1>
+        </div>
+    );
+};
--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,27 +1,36 @@
 import { useEffect, useState } from "react";
+import { Card } from "./Card";
 
 export const GetData = () => {
     const url = "https://pokeapi.co/api/v2/pokemon";
 
     const [load, setLoad] = useState(true);
-    const [allData, setAllData] = useState(null);
-    const [data, setdata] = useState(null);
+    const [allPokemon, setAllPokemon] = useState(null);
+    // const [data, setdata] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            let allData = await getAllData(url);
-            loadData(allData);
+            let _allPokemon = await getAllPokemon(url);
+            console.log(_allPokemon);
+            return _allPokemon;
         };
+        setAllPokemon(fetchData());
         setLoad(false);
-        setAllData(fetchData());
+        // loadPokemon(allPokemon);
     }, []);
 
-    //   loadData(allData);
-
-    return <div>{load ? <h1>loading...</h1> : <h1>Data:</h1>}</div>;
+    return (
+        <div>
+            {load ? (
+                <h1>loading...</h1>
+            ) : (
+                <h1>{/* <Card pokemon={pokemon} /> */}</h1>
+            )}
+        </div>
+    );
 };
 
-const getAllData = (url) => {
+const getAllPokemon = (url) => {
     return new Promise((resolve, reject) => {
         fetch(url)
             .then((res) => res.json())
@@ -30,23 +39,22 @@ const getAllData = (url) => {
     });
 };
 
-const getData = (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                resolve(data.results);
-            })
-            .catch((error) => reject(error));
-    });
-};
+// const getData = (url) => {
+//     return new Promise((resolve, reject) => {
+//         fetch(url)
+//             .then((res) => res.json())
+//             .then((data) => resolve(data.results))
+//             .catch((error) => reject(error));
+//     });
+// };
 
-const loadData = (allData) => {
-    let loadData = Promise.all(
-        allData.map((value) => {
-            let dataRecord = getData(value.url);
-            return dataRecord;
+const loadPokemon = async (allPokemon) => {
+    let loadData = await Promise.all(
+        allPokemon.map((value) => {
+            console.log(value.url);
+            // let dataRecord = getData(value.url);
+            // return dataRecord;
         })
     );
+    console.log(loadData);
 };
