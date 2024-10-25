f628043ed9be4784e2dc1b2638a396fcc69bb7c2
FujiiAmon <amondealwis@gmail.com>
2024-10-25 18:28:17 +0900

    これはダメ 多分fetchを待たずに次へ行ってる

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,9 @@
-export const Card = ({ pokemon }) => {
+export const Card = (props) => {
+    const { name, sprites } = props;
     return (
         <div>
-            <h1>Card</h1>
+            <h1>{name}</h1>
+            <img src={sprites.front_default} alt={name} />
         </div>
     );
 };
--- src/components/GetData.jsx
+++ src/components/GetData.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Card } from "./Card";
 
 export const GetData = () => {
     const url = "https://pokeapi.co/api/v2/pokemon";
@@ -9,23 +8,10 @@ export const GetData = () => {
     const [pokemon, setPokemon] = useState(null);
 
     useEffect(() => {
-        const fetchAllData = async () => {
-            let _allPokemon = await getAllPokemon(url);
-            setAllPokemon(_allPokemon);
-        };
-        fetchAllData();
-        // console.log(allPokemon);
-        const fetchData = async () => {
-            let _pokemon = await Promise.all(
-                allPokemon.map((value) => {
-                    getPokemon(value.url);
-                })
-            );
-            setPokemon(_pokemon);
-        };
-        fetchData();
-        console.log(pokemon);
-        setLoad(false);
+        fetch(url)
+            .then((val) => val.json())
+            .then((val) => setAllPokemon(val.results));
+        console.log(allPokemon);
     }, []);
 
     return (
@@ -33,7 +19,8 @@ export const GetData = () => {
             {load ? (
                 <h1>loading...</h1>
             ) : (
-                <h1>{/* <Card pokemon={pokemon} /> */}</h1>
+                <h1>Data</h1>
+                // <Card name={pokemon.name} sprites={pokemon.sprites} />
             )}
         </div>
     );
@@ -43,7 +30,7 @@ const getAllPokemon = (url) => {
     return new Promise((resolve, reject) => {
         fetch(url)
             .then((res) => res.json())
-            .then((data) => resolve(data.results))
+            .then((data) => resolve(data))
             .catch((error) => reject(error));
     });
 };
