224f75e9cb1343f80a51e11a74bb2a07a82e6c2f
FujiiAmon <amondealwis@gmail.com>
2024-11-01 03:01:04 +0900

    やり直し

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: process.env.GITHUB_PAGES // この行を追加
+        ? "poke-pedia" // この行を追加
+        : "./", // この行を追加
+    plugins: [react()],
+});
